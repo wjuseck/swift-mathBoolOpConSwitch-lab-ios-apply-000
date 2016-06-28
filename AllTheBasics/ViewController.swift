3f24a140dbe5fec667453b792bbfd3ff793e8f71
JimCampagno <jimcampagno@gmail.com>
2016-06-28 09:47:52 -0400

    add xcode project

---
+++ AllTheBasics/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AllTheBasics
+//
+//  Created by James Campagno on 6/28/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    
+
+
+}
+
