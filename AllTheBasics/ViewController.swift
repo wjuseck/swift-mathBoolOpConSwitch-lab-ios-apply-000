aaa1ef503f8e84c87c5bcbef382c315def672b11
JimCampagno <jimcampagno@gmail.com>
2016-09-14 14:24:38 -0400

    Remove Solution

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -17,41 +17,5 @@ class ViewController: UIViewController {
     
    // Implement your functions here
 
-    func averageIsAbove(_ a: Double, _ b: Double, _ c: Double) -> Bool {
-        
-        return ((a + b + c) / 3.0) > 75.0
-        
-    }
-    
-    func passwordCombo(username: String, password: Int) -> String {
-        
-        switch username {
-        case "Jerry", "Elaine", "Michael":
-            if password % 3 == 0 { return "Welcome!" }
-            return "Access Denied"
-        default:
-            return "Access Denied"
-        }
-        
-    }
-    
-    func describe(emoji: String) -> String {
-        
-        switch emoji {
-        case "💋":
-            return "Kiss"
-        case "🐈":
-            return "Cat"
-        case "🐢":
-            return "Turtle"
-        case "🍕":
-            return "Pizza"
-        case "👻":
-            return "Ghost"
-        default:
-            return "Unknown"
-        }
-        
-    }
-    
+
 }
