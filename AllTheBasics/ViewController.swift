d1a9f7d392ae822124382af80c0acac54db30d36
wjuseck <will.juseck@gmail.com>
2017-02-07 16:10:37 -0500

    Got errors in question 3 but I think my code is correct

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -13,11 +13,45 @@ class ViewController: UIViewController {
     
     
     
-    
-    // Implement your functions here
-    
-    
-    
-    
+func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+    if a + b + c / 3 >= 75 {
+        return true
+    } else {
+        return false
+    }
+}
 
+func passwordCombo(username: String, password: Int) -> String {
+    if username == "Jerry" {
+        }
+    if username == "Elaine" {
+        }
+    if username == "Michael" {
+        }
+    if (password % 3 == 0) {
+        return "Welcome!"
+    }
+    else {
+    return "Access Denied"}
+        }
+}
+
+func describe(emoji: String) -> String {
+    if emoji == "💋" {
+        return "Kiss"
+    }
+    if emoji == "🐈" {
+        return "Cat"
+    }
+    if emoji == "🐢" {
+        return "Turtle"
+    }
+    if emoji == "🍕" {
+        return "Pizza"
+    }
+    if emoji == "👻" {
+        return "Ghost"
+    } else {
+        return "Unknown"
+    }
 }
